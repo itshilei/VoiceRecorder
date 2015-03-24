002fa792d37dfb2c7f705afc496206ca44499614
shi <itshilei@gmail.com>
2015-03-24 15:31:54 +0100

    Add navigation controller and slow play button

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    override func viewWillAppear(animated: Bool) {
+        //hide the stop button
+        stopButton.hidden=true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,8 +30,15 @@ class ViewController: UIViewController {
     @IBAction func RecordAudio(sender: UIButton) {
         //TODO: Show text " Recording in progress"
         recordingInProgress.hidden = false
+        
+        //show stopButton
+        stopButton.hidden=false
         //Record the user's voice
+        
         println("in recording")
+    }
+    @IBAction func stopAudio(sender: UIButton) {
+        recordingInProgress.hidden = true
     }
 }
 
